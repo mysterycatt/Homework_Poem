97efeef0f75c7d8169daa987191f2d925c398d5b
Pei Wu <peiwu@peidemacbook-air.local>
2018-07-17 19:35:12 +0800

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Pei Wu on 2018/7/17.
+//  Copyright © 2018年 Pei Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
