8531c4bb0bbcea3031e151defbc14547e425b1ee
Pei Wu <peiwu@peidemacbook-air.local>
2018-07-23 09:00:22 +0800

    added up Homework 2

---
+++ TabbedApp/TabbedApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TabbedApp
+//
+//  Created by Pei Wu on 2018/7/22.
+//  Copyright © 2018年 Pei Wu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TabbedApp/TabbedApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TabbedApp
+//
+//  Created by Pei Wu on 2018/7/22.
+//  Copyright © 2018年 Pei Wu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
