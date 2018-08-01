7250805a39137dd91397b0ff4e766b622b65fa74
Pei Wu <peiwu@peidemacbook-air.local>
2018-08-01 11:08:49 +0800

    Lecture III & Homework #4 Poem

---
+++ Lecture III/MyPlayground.playground/Contents.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+var str = "Hello, playground"
+3 + 3
+5 + 5
+1*1 + 2*2 + 3*3 + 4*4 + 5*5
+"Pei Wu"
+"Hello" + "World"
+"1" + "1"
+1 + 1
+"0931034007"
+// 註解
+/*
+ 多行註解
+*/
+let numberofDogs = 6
+let numberofCats = 7
+numberofDogs + numberofCats
+numberofDogs
+let scoreofMath = 99.00
+let scoreofEnglish = 97.00
+let scoreofChinese = 97.00
+(scoreofMath+scoreofEnglish+scoreofChinese)/3
+var ABC = 10
+ABC = 11
+var number1 = 1
+var number2 = 20
+var tempnumber = number1
+number1 = number2
+number2 = tempnumber
+number1
+number2
+var age = 18
+age + 1
+age
+age = age+1
+age
+var age1 = 18
+var age2 = 20
+age1 = age2
+age1 = 17
+age2
+let pei = "good"
+print("PeiWu")
+var sum = 1+2+3
+print(sum)
+// \n指換行
+print("Pei", "Wu")
+var goodman = "Pei Wu"
+goodman.append("Hi")
+goodman.count
+var number = Int()
+var emptyMessage = String()
